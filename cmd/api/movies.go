83169c4110b8c3bd54131f6695394ed566c1f3e7
Muluneh Derese <muluneh.derese@protonmail.com>
2023-01-27 05:38:49 +0300

    feat: use JSON format

---
+++ cmd/api/movies.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "create a new movie\n")
+}
+
+func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
+	id, err := app.readIDParam(r)
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "show the details of the movie %d\n", id)
+}
