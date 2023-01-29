00d465876758510321eaecb054667499864548dd
Muluneh Derese <muluneh.derese@protonmail.com>
2023-01-29 12:36:38 +0300

    feat: use envelope while writing json

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -21,8 +21,10 @@ func (app *application) readIDParam(r *http.Request) (int64, error) {
 	return id, nil
 }
 
-func (app *application) writeJSON(w http.ResponseWriter, status int, data interface{}, headers http.Header) error {
-	js, err := json.Marshal(data)
+type envelope map[string]interface{}
+
+func (app *application) writeJSON(w http.ResponseWriter, status int, data envelope, headers http.Header) error {
+	js, err := json.MarshalIndent(data, "", "\t")
 	if err != nil {
 		return err
 	}
