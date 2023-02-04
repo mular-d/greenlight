435450942e9f6da532f4b09e0d2fc92309565aca
Muluneh Derese <muluneh.derese@protonmail.com>
2023-02-04 15:03:42 +0300

    feat: sorting lists

--- internal/data/filters.go
+++ internal/data/filters.go
@@ -1,12 +1,33 @@
 package data
 
-import "github.com/targerian1999/greenlight/internal/validator"
+import (
+	"strings"
+
+	"github.com/targerian1999/greenlight/internal/validator"
+)
 
 type Filters struct {
 	Page         int
 	PageSize     int
 	Sort         string
 	SortSafelist []string
+}
+
+func (f Filters) sortColumn() string {
+	for _, safeValue := range f.SortSafelist {
+		if f.Sort == safeValue {
+			return strings.TrimPrefix(f.Sort, "-")
+		}
+	}
+
+	panic("unsafe sort parameter: " + f.Sort)
+}
+
+func (f Filters) sortDirection() string {
+	if strings.HasPrefix(f.Sort, "-") {
+		return "DESC"
+	}
+	return "ASC"
 }
 
 func ValidateFilters(v *validator.Validator, f Filters) {
