6757ff1183795e693906fc0eeed44ea73f8f10cb
Muluneh Derese <muluneh.derese@protonmail.com>
2023-02-05 14:38:57 +0300

    feat: set up user model

--- internal/data/models.go
+++ internal/data/models.go
@@ -7,15 +7,17 @@ import (
 
 var (
 	ErrRecordNotFound = errors.New("record not found")
-	ErrEditConflict = errors.New("edit conflict")
+	ErrEditConflict   = errors.New("edit conflict")
 )
 
 type Models struct {
 	Movies MovieModel
+	Users  UserModel
 }
 
 func NewModels(db *sql.DB) Models {
 	return Models{
 		Movies: MovieModel{DB: db},
+		Users:  UserModel{DB: db},
 	}
 }
